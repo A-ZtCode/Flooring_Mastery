1126c5e20686782eb768df4fd7b6271a20b6c1f8
AltheaZeta <86517586+AltheaZeta@users.noreply.github.com>
2023-08-11 13:22:56 +0100

    New Commit
    Changes to TaxDaoImpl

--- src/main/java/dao/TaxDaoImpl.java
+++ src/main/java/dao/TaxDaoImpl.java
@@ -2,107 +2,71 @@ package dao;
 
 import modelDTO.Tax;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
+import java.util.*;
 
 public class TaxDaoImpl implements TaxDao {
 
     private final Map<String, Tax> taxes = new HashMap<>(); // in-memory storage
-    private final String FILE_PATH = "taxes.txt"; // Name of the file that contains tax data
+    private final String FILE_PATH = "Taxes.txt"; // Name of the file that contains tax data
+    private final String BACKUP_PATH = FILE_PATH + ".bak"; // Backup
 
+    public TaxDaoImpl() {
+        // Load taxes into the in-memory map during instantiation
+        loadTaxesFromFile().forEach(tax -> taxes.put(tax.getStateAbbreviation(), tax));
+    }
 
+    // Fetch tax information by state abbreviation
     @Override
     public Tax getTaxByState(String state) {
-        List<Tax> taxes = loadTaxesFromFile();
-        for (Tax tax : taxes) {
-            if (tax.getStateAbbreviation().equals(state)) {
-                return tax;
-            }
-        }
-        return null; // Return null if no matching tax is found
+        loadTaxesFromFile(); // Load taxes from the file to the in-memory storage
+        return taxes.get(state); // Fetch the tax from the in-memory storage
     }
 
+
+    // Fetch all tax information
     @Override
     public List<Tax> getAllTaxes() {
-        return loadTaxesFromFile();
+        return new ArrayList<>(taxes.values()); // Return all tax values from the in-memory storage
     }
 
-
+    // Add a new tax entry
     @Override
     public Tax addTax(Tax newTax) {
-        List<Tax> taxes = loadTaxesFromFile();
-
-        // Check if the tax entry for the provided state already exists
-        for (Tax tax : taxes) {
-            if (tax.getStateAbbreviation().equals(newTax.getStateAbbreviation())) {
-                throw new DaoException("Tax for the state " + newTax.getStateAbbreviation() + " already exists!");
-            }
+        if (taxes.containsKey(newTax.getStateAbbreviation())) {
+            throw new DaoException("Tax for the state " + newTax.getStateAbbreviation() + " already exists!");
         }
-
-        // If not, add it to the list
-        taxes.add(newTax);
-
-        // Then, save the updated list back to the file
-        saveTaxesToFile(taxes);
+        taxes.put(newTax.getStateAbbreviation(), newTax); // Add tax to in-memory storage
+        saveTaxesToFile(); // Save the updated taxes back to the file
         return newTax;
     }
+
+    // Update an existing tax entry
     @Override
     public boolean updateTax(Tax updatedTax) {
-        List<Tax> taxes = loadTaxesFromFile();
-        int index = -1;
-
-        // Check if the tax entry for the provided state exists
-        for (int i = 0; i < taxes.size(); i++) {
-            if (taxes.get(i).getStateAbbreviation().equals(updatedTax.getStateAbbreviation())) {
-                index = i;
-                break;
-            }
+        if (!taxes.containsKey(updatedTax.getStateAbbreviation())) {
+            return false;  // Tax entry not found
         }
-
-        if (index == -1) {
-            return false;  // Tax entry not found, return false
-        }
-
-        // Update the tax details
-        taxes.set(index, updatedTax);
-
-        // Then, save the updated list back to the file
-        saveTaxesToFile(taxes);
-        return true;  // Update was successful, return true
+        taxes.put(updatedTax.getStateAbbreviation(), updatedTax); // Update tax in in-memory storage
+        saveTaxesToFile(); // Save the updated taxes back to the file
+        return true;  // Update was successful
     }
 
-
+    // Remove a tax entry by state abbreviation
     @Override
     public boolean removeTaxByState(String state) {
-        List<Tax> taxes = loadTaxesFromFile();
-
-        // Use Java 8 streams to filter out the tax entry for the provided state
-        List<Tax> updatedTaxes = taxes.stream()
-                .filter(tax -> !tax.getStateAbbreviation().equals(state))
-                .collect(Collectors.toList());
-
-        if (taxes.size() == updatedTaxes.size()) {
+        if (!taxes.containsKey(state)) {
             return false;  // No tax entry was removed
         }
-
-        // Save the updated list without the removed tax entry back to the file
-        saveTaxesToFile(updatedTaxes);
+        taxes.remove(state);
+        saveTaxesToFile(); // Save the updated taxes back to the file
         return true;  // Tax entry was removed successfully
     }
 
-
-    // Helper method to load taxes from the file
+    // Helper method to load taxes from the file to in-memory storage
     private List<Tax> loadTaxesFromFile() {
-        List<Tax> taxes = new ArrayList<>();
+        List<Tax> fileTaxes = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
             String line;
             while ((line = reader.readLine()) != null) {
@@ -111,23 +75,57 @@ public class TaxDaoImpl implements TaxDao {
                     String stateAbbreviation = parts[0].trim();
                     String stateName = parts[1].trim();
                     BigDecimal taxRate = new BigDecimal(parts[2].trim());
-                    taxes.add(new Tax(stateAbbreviation, stateName, taxRate));
+                    fileTaxes.add(new Tax(stateAbbreviation, stateName, taxRate)); // Add tax to in-memory storage
                 }
             }
         } catch (IOException ex) {
             System.err.println("Error reading taxes from file: " + ex.getMessage());
         }
-        return taxes;
+        return fileTaxes;
     }
 
     // Helper method to save taxes to the file
-    private void saveTaxesToFile(List<Tax> taxes) {
+    private void saveTaxesToFile() {
+        backupFile();
         try (FileWriter writer = new FileWriter(FILE_PATH)) {
-            for (Tax tax : taxes) {
+            for (Tax tax : taxes.values()) {
                 writer.write(tax.getStateAbbreviation() + "," + tax.getStateName() + "," + tax.getTaxRate() + "\n");
             }
         } catch (IOException ex) {
             System.err.println("Error writing taxes to file: " + ex.getMessage());
+            restoreBackup();  // if there's an error, restore from backup
+        }
+    }
+
+    // Backup the current data file
+    private void backupFile() {
+        File sourceFile = new File(FILE_PATH);
+        File backupFile = new File(BACKUP_PATH);
+        try (FileInputStream fis = new FileInputStream(sourceFile);
+             FileOutputStream fos = new FileOutputStream(backupFile)) {
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = fis.read(buffer)) != -1) {
+                fos.write(buffer, 0, bytesRead);
+            }
+        } catch (IOException ex) {
+            System.err.println("Error creating backup: " + ex.getMessage());
+        }
+    }
+
+    // Restore from the backup file
+    private void restoreBackup() {
+        File backupFile = new File(BACKUP_PATH);
+        File sourceFile = new File(FILE_PATH);
+        try (FileInputStream fis = new FileInputStream(backupFile);
+             FileOutputStream fos = new FileOutputStream(sourceFile)) {
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = fis.read(buffer)) != -1) {
+                fos.write(buffer, 0, bytesRead);
+            }
+        } catch (IOException ex) {
+            System.err.println("Error restoring backup: " + ex.getMessage());
         }
     }
 }
